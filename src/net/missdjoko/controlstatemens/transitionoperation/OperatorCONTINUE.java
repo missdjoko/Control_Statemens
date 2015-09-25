f41974f4a666c9a5bafd3a80c5a5f019fd44fb6a
Miss_djoko <miss_djoko@outlook.com>
2015-09-25 16:05:39 +0300

    project export

--- src/net/missdjoko/controlstatemens/transitionoperation/OperatorCONTINUE.java
+++ src/net/missdjoko/controlstatemens/transitionoperation/OperatorCONTINUE.java
@@ -5,6 +5,25 @@ package net.missdjoko.controlstatemens.transitionoperation;
  */
 public class OperatorCONTINUE {
     protected static void main(){
+    //оператор continue вызывает передачу управления непосредственно условному
+    //    выражению, управляющему циклом. В цикле for управление передается вначале
+    //   итерационной части цикла for , а затем условному выражению. Во всех трех
+    //    видах циклов любой промежуточный код пропускается
+    // break- делает выход из текущего цикла и передает управление следующему коду или оператору,
+    // continue- запускает новую итерацию этого же самого цикла
 
+         for ( int i = 0 ; i <10; i ++ ) {
+            for (int j = 0; j < 10; j++) {
+                if (j > i) {
+                    System.out.println();
+                    continue ;
+                }
+                System.out.print(" " + (i * j));
+
+
+                }
+                System.out.println();
+            }
+        }
     }
-}
+
--- src/net/missdjoko/controlstatemens/transitionoperation/TransitionOperators.java
+++ src/net/missdjoko/controlstatemens/transitionoperation/TransitionOperators.java
@@ -4,10 +4,12 @@ package net.missdjoko.controlstatemens.transitionoperation;
  * Created by Miss_Djoko on 09.09.2015.
  */
 public class TransitionOperators {
+    // операторы перехода return, break, continue, trow должны всегда идти последними в блоке
     public static void main(){
-      //  OperatorBREAKForOutFromCycle.main();
+       //  OperatorBREAKForOutFromCycle.main();
        // OperatorBREAKAsGoTo.main();
-        OperatorCONTINUE.main();
+       //  OperatorCONTINUE.main();
+        OperatorRETURN.main();
 
 
     }
